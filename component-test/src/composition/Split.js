a279dc0d278c91ebc649566e992c82b048acaf8c
John Azzaro <jazzaro@temple.edu>
2020-03-09 17:03:42 -0700

    component improvement

--- component-test/src/composition/Split.js
+++ component-test/src/composition/Split.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 function Split() {
   return (
-    <div classname='split'></div>
+    <div classname='split'>
+      This is the content of the split component.
+    </div>
   );
 }
